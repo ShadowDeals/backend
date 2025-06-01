69a62e703dc231d3c81cdd5f6c6092e3a177712f
Kirill Simovin <volce.chat@mail.ru>
2025-06-01 16:46:27 +0300

    [BACKEND main] [add] region_id column to band table

--- src/main/java/com/shadow/deals/band/entity/Band.java
+++ src/main/java/com/shadow/deals/band/entity/Band.java
@@ -2,10 +2,13 @@ package com.shadow.deals.band.entity;
 
 import com.shadow.deals.base.entity.BaseIdEntity;
 import com.shadow.deals.user.main.entity.User;
+import com.shadow.deals.user.region.entity.Region;
 import com.shadow.deals.util.constant.CommonConstantHolder;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -26,6 +29,10 @@ public class Band extends BaseIdEntity {
     @OneToOne
     @JoinColumn(name = "don_id", referencedColumnName = "id")
     private User don;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", referencedColumnName = "id")
+    private Region region;
 
     @OneToMany(mappedBy = "band")
     private Set<User> workers;
