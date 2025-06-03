f1e4df6f645ae16002f9309e3446d8acbe385225
Kirill Simovin <volce.chat@mail.ru>
2025-06-03 20:33:43 +0300

    [BACKEND main] [add] getWorkersByType

---
+++ src/main/java/com/shadow/deals/band/main/controller/BandController.java
@@ -0,0 +1,28 @@
+package com.shadow.deals.band.main.controller;
+
+import com.shadow.deals.band.main.service.BandService;
+import com.shadow.deals.user.main.dto.response.BandWorkerResponseDTO;
+import com.shadow.deals.user.role.enums.UserRoleName;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
+import jakarta.annotation.security.RolesAllowed;
+import lombok.RequiredArgsConstructor;
+
+import java.util.TreeSet;
+import java.util.UUID;
+
+/**
+ * @author Kirill "Tamada" Simovin
+ */
+@Controller("/band")
+@RequiredArgsConstructor
+public class BandController {
+    private final BandService bandService;
+
+    @Get("/workers")
+    @RolesAllowed({"Дон", "Администратор"})
+    public TreeSet<BandWorkerResponseDTO> getWorkersByType(@QueryValue(value = "bandId") UUID bandId, @QueryValue(value = "userRole") UserRoleName userRole) {
+        return bandService.getWorkersByType(bandId, userRole);
+    }
+}
