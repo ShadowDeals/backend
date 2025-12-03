f539ab90ab0d84e7a313e0deab52c3869715c3e2
Kirill Simovin <volce.chat@mail.ru>
2025-12-03 18:31:19 +0300

    [main] [add] test class that extend base abstract class

---
+++ src/test/java/com/shadow/deals/TestTest.java
@@ -0,0 +1,13 @@
+package com.shadow.deals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Kirill "Tamada" Simovin
+ */
+public class TestTest extends BaseTestContainerTest {
+    @Test
+    void testItWorks() {
+        System.out.println("Testing it's works");
+    }
+}
