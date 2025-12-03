d116a39827448488e6d604d15d31f1b0dfa5c70e
Kirill Simovin <volce.chat@mail.ru>
2025-12-03 18:30:32 +0300

    [main] [add] base class for test containers

---
+++ src/test/java/com/shadow/deals/BaseTestContainerTest.java
@@ -0,0 +1,38 @@
+package com.shadow.deals;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Map;
+
+/**
+ * @author Kirill "Tamada" Simovin
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@MicronautTest
+public abstract class BaseTestContainerTest implements TestPropertyProvider {
+    private static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
+            .withDatabaseName("shadow")
+            .withUsername("postgres")
+            .withPassword("postgrespass")
+            .withReuse(true)
+            .withExposedPorts(5432);
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @Override
+    public @NonNull Map<String, String> getProperties() {
+        return Map.of(
+                "datasources.default.url", postgreSQLContainer.getJdbcUrl(),
+                "datasources.default.username", postgreSQLContainer.getUsername(),
+                "datasources.default.password", postgreSQLContainer.getPassword(),
+                "datasources.default.driver-class-name", "org.postgresql.Driver"
+        );
+    }
+}
