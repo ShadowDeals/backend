599bb602084a17ca09d8a55d80c69a2869d2fbda
Kirill Simovin <volce.chat@mail.ru>
2025-12-19 21:53:57 +0300

    [main] [edit] move auth/create user vie requests to base class

--- src/test/java/com/shadow/deals/base/BaseAuthTestContainerTest.java
+++ src/test/java/com/shadow/deals/base/BaseAuthTestContainerTest.java
@@ -1,14 +1,21 @@
 package com.shadow.deals.base;
 
+import com.shadow.deals.auth.dto.request.SignInRequestDTO;
 import com.shadow.deals.auth.dto.request.SignUpRequestDTO;
-import com.shadow.deals.auth.service.AuthService;
+import com.shadow.deals.auth.dto.response.TokenResponseDTO;
 import com.shadow.deals.region.enums.RegionName;
 import com.shadow.deals.user.main.service.UserService;
 import com.shadow.deals.user.role.enums.UserRoleName;
 import com.shadow.deals.util.CommonUtils;
 import com.shadow.deals.util.TestUtils;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import jakarta.inject.Inject;
+import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * @author Kirill "Tamada" Simovin
@@ -16,10 +23,13 @@ import java.util.UUID;
 public abstract class BaseAuthTestContainerTest extends BaseTestContainerTest {
 
     @Inject
-    protected AuthService authService;
+    @Client("/")
+    protected HttpClient client;
 
     @Inject
     private UserService userService;
+
+    protected final static String PASSWORD = "qweewq";
 
     protected String createDon() {
         return createUser("volce.chat@mail.ru", UserRoleName.DON, RegionName.MOSCOW_REGION);
@@ -29,20 +39,70 @@ public abstract class BaseAuthTestContainerTest extends BaseTestContainerTest {
         return createUser(email, role, RegionName.MOSCOW_REGION);
     }
 
-    protected String createUser(String email, UserRoleName role, RegionName regionName) {
-        authService.signup(
-            new SignUpRequestDTO(
-                "TEST",
-                "TEST",
-                "TEST",
-                "TEST",
-                email,
-                role,
-                regionName
-            )
+    protected String createUser(String email, UserRoleName role, RegionName region) {
+        try {
+            HttpResponse<TokenResponseDTO> response = client.toBlocking().exchange(
+                HttpRequest.POST(
+                    "/auth/signin",
+                    new SignInRequestDTO(
+                        email,
+                        PASSWORD,
+                        false
+                    )
+                ),
+                TokenResponseDTO.class
+            );
+
+            TokenResponseDTO tokenBody = getFromResponseBody(response);
+            return tokenBody.getAccessToken();
+        } catch (Exception e) {
+            return createUserRequest(email, role, region);
+        }
+    }
+
+    private String createUserRequest(String email, UserRoleName role, RegionName region) {
+
+        SignUpRequestDTO dto = new SignUpRequestDTO(
+            "TEST",
+            "TEST",
+            "TEST",
+            PASSWORD,
+            email,
+            role,
+            region
         );
 
-        return authService.confirmEmail(CommonUtils.generateUUIDFromString(email)).getAccessToken();
+        HttpResponse<String> emailResponse = Assertions.assertDoesNotThrow(() -> client.toBlocking().exchange(
+                HttpRequest.POST("/auth/signup", dto),
+                String.class
+            )
+        );
+        String emailBody = getFromResponseBody(emailResponse);
+
+        String activationCode = CommonUtils.generateUUIDFromString(emailBody);
+
+        HttpResponse<TokenResponseDTO> response = Assertions.assertDoesNotThrow(() -> client.toBlocking().exchange(
+                HttpRequest.PUT("/auth/confirm/email?code=" + activationCode, null),
+                TokenResponseDTO.class
+            )
+        );
+        TokenResponseDTO tokenBody = getFromResponseBody(response);
+
+        return tokenBody.getAccessToken();
+    }
+
+    protected <R> R getFromResponseBody(HttpResponse<R> response) {
+        if (response == null) {
+            Assertions.fail("response is null");
+        }
+
+        Optional<R> bodyOpt = response.getBody();
+        Assertions.assertTrue(bodyOpt.isPresent());
+
+        R body = bodyOpt.get();
+        Assertions.assertNotNull(body);
+
+        return body;
     }
 
     protected UUID getUserIdFromAccessToken(String accessToken) {
